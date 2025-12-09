721cb6c86e07f626def13505f6f155816759b01c
Zoldinn <leonfoffano@outlook.fr>
2025-12-09 15:53:54 +0100

    fixed another error

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -13,9 +13,9 @@ if [ ! -d "/var/lib/mysql/$SQL_DATABASE" ]; then
 	mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';" # create wordpress user
 	mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';"
 	mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" # change root's password
-	mariadb -u root -p "${SQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+	mariadb -u root -p"${SQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
 
-	mysqladmin -u root -p "${SQL_ROOT_PASSWORD}" shutdown # down mariadb because configuration finished
+	mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" shutdown # down mariadb because configuration finished
 	
 	echo "DB Created !"
 else
